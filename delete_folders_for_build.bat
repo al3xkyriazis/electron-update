594bdea9f0f53624e8a45f487c42b6d0eb6cc08f
al3xkyriazis <50414979+al3xkyriazis@users.noreply.github.com>
2019-05-08 18:05:39 +0300

    Add files via upload

---
+++ delete_folders_for_build.bat
@@ -0,0 +1,4 @@
+rmdir C:\projects\mdt\repo\mdt\mdt-tool\mdt-tool-interpreter\target /S /Q
+rmdir C:\projects\mdt\repo\mdt\mdt-tool\mdt-tool-gui\target\apache-tomee /S /Q
+cd C:\projects\mdt\repo\mdt\mdt-tool\mdt-tool-gui\target\ 
+del *.jar
